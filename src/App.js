55d82117a3ac0045c768adab94d6d215970cafca
Sam Mahler <mahler.sam@gmail.com>
2020-11-13 16:51:56 -0600

    import bulma from node packages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import './App.css';
-import './bulma.min.css'
+import 'bulma/css/bulma.css';
 
 function App() {
   return (
     <div className="App">
-   <h1 className="title">Bulma</h1>
-<p className="subtitle">
-  Modern CSS framework based on{' '}
-  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
-    Flexbox
-  </a>
-</p>
 
-<div className="field">
-  <div className="control">
-    <input className="input" type="text" placeholder="Input" />
-  </div>
-</div>
+      <h1 className="title">Bulma</h1>
+      <p className="subtitle">
+        Modern CSS framework based on{' '}
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
+          Flexbox
+        </a>
+      </p>
 
-<div className="field">
-  <p className="control">
-    <span className="select">
-      <select>
-        <option>Select dropdown</option>
-      </select>
-    </span>
-  </p>
-</div>
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Input" />
+        </div>
+      </div>
 
-<div className="buttons">
-  <a className="button is-primary" href="https://www.google.com">Primary</a>
-  <a className="button is-link" href="https://www.google.com">Link</a>
-</div>
-</div>
+      <div className="field">
+        <p className="control">
+          <span className="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+
+      <div className="buttons">
+        <a className="button is-primary" href="https://www.google.com">Primary</a>
+        <a className="button" href="https://www.google.com" disabled>Link</a>
+      </div>
+
+      <div className="columns">
+        <div className="column">1</div>
+        <div className="column">2</div>
+        <div className="column">3</div>
+        <div className="column">4</div>
+        <div className="column">5</div>
+      </div>
+
+    </div>
   );
 }
 
